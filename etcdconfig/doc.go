66d2ff182bc6625be97019cd00fe1a4fbc7ec627
Sam Dodrill <sam@opdemand.com>
2014-08-23 14:08:44 -0400

    docs(etcdconfig): update documentation

--- etcdconfig/doc.go
+++ etcdconfig/doc.go
@@ -5,14 +5,22 @@ this package as a primary configuration source also have a "seed" option or
 similar to ensure that etcd does not have any missing keys from the first
 launch.
 
-Currently etcdconfig supports string and boolean variables from etcd. More
+Currently etcdconfig supports strings, string->string maps and boolean variables from etcd. More
 will be added as time goes on. Usage is very simple:
 
     type Config struct {
-    	Foo string `etcd:"/test/foo"`
+    	Foo string            `etcd:"/test/foo"`
+    	Bar bool              `etcd:"/test/bar"`
+    	Baz map[string]string `etcd:"/test/baz"`
     }
 
-A string will always have its value copied verbatim but a boolean merely checks
-for the presence or absense of a key.
+    Demarshall(etcd, someStruct)
+
+A string will always have its value copied verbatim.
+
+A boolean merely checks for the presence or absense of a key.
+
+With maps, the directory tree will be recursively walked and all subdirectories and
+their keys will be added as-is.
 */
 package etcdconfig
