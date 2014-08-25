e827fa6d71e368cd2dd53405ed93e414ac6b68fa
Sam Dodrill <sam@opdemand.com>
2014-08-25 13:33:39 -0400

    fix(builder/config): use the controller struct in deis bindings

--- builder/config.go
+++ builder/config.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/Xe/flitter/deis"
 	"github.com/Xe/flitter/etcdconfig"
 	"github.com/coreos/go-etcd/etcd"
 )
@@ -9,38 +10,26 @@ import (
 // strings from etcd. This uses code in another part of the repository to marshall
 // and demarshall config in and out of etcd.
 type Config struct {
-	SlugbuilderImage   string `etcd:"/deis/slugbuilder/image"`
-	SlugrunnerImage    string `etcd:"/deis/slugrunner/image"`
-	DockerfileShim     string `etcd:"/deis/builder/dockerfileshim"`
-	ControllerProtocol string `etcd:"/deis/controller/protocol"`
-	ControllerHost     string `etcd:"/deis/controller/host"`
-	ControllerPort     string `etcd:"/deis/controller/port"`
-	ControllerBuildKey string `etcd:"/deis/controller/buildKey"`
-	etcd               *etcd.Client
-	updates            chan *etcd.Response
-	stop               chan bool
+	SlugbuilderImage string `etcd:"/deis/slugbuilder/image"`
+	SlugrunnerImage  string `etcd:"/deis/slugrunner/image"`
+	DockerfileShim   string `etcd:"/deis/builder/dockerfileshim"`
+	Controller       *deis.Controller
+	etcd             *etcd.Client
+	updates          chan *etcd.Response
+	stop             chan bool
 }
 
 // NewConfig allocates and retuens a config structure for builder. It also seeds
 // the values from etcd.
-func NewConfig(prefix, uplink string) (c *Config) {
+func NewConfig(uplink string) (c *Config) {
 	c = &Config{
-		etcd:    etcd.NewClient([]string{uplink}),
-		updates: make(chan *etcd.Response, 10),
-		stop:    make(chan bool),
+		etcd:       etcd.NewClient([]string{uplink}),
+		updates:    make(chan *etcd.Response, 10),
+		stop:       make(chan bool),
+		Controller: deis.NewControllerEtcd(uplink),
 	}
 
 	etcdconfig.Demarshal(c.etcd, c)
-
-	c.etcd.Watch(prefix, 0, true, c.updates, c.stop)
-
-	go func() {
-		for update := range c.updates {
-			_ = update // TODO: replace me with a better method
-
-			etcdconfig.Demarshal(c.etcd, c)
-		}
-	}()
 
 	return
 }
