c617d52b6da334c9faa4ae6d342e5e2a677c09a1
Sam Dodrill <sam@opdemand.com>
2014-08-26 19:23:43 -0400

    feat(cloudchaser): show envvars if precommit hook

--- cloudchaser/main.go
+++ cloudchaser/main.go
@@ -1,17 +1,38 @@
 package main
 
 import (
+	"fmt"
+	"log"
 	"os"
 
 	"github.com/Xe/flitter/builder/output"
+	"github.com/docopt/docopt-go"
 )
 
 func main() {
-	output.WriteHeader("Receiver\n")
+	usage := `Cloudchaser Builder Sentry
 
-	output.WriteHeader("Environment:")
+Usage:
+  cloudchaser <command> <revision>
 
-	for _, val := range os.Environ() {
-		output.WriteData(val)
+Commands:
+  pre   runs precommit checks, exits with 1 if a failure happens
+  post  kicks off builder
+`
+	arguments, err := docopt.Parse(usage, nil, true, "Flitter Sentry 0.1", false)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(arguments)
+
+	if arguments["<command>"].(string) == "pre" {
+		output.WriteHeader("Receiver\n")
+
+		output.WriteHeader("Environment:")
+
+		for _, val := range os.Environ() {
+			output.WriteData(val)
+		}
 	}
 }
