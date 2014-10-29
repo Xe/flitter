140ec7376b7983b19dab95c5f063259acabe9bb8
Sam Dodrill <xena@yolo-swag.com>
2014-10-29 10:57:46 -0700

    lagann/datatypes: add documentation

---
+++ lagann/datatypes/doc.go
@@ -0,0 +1,6 @@
+/*
+Package datatypes is a set of simple structs for lagann and other applications that communicate with it.
+Consider this the schema documentation for creating a program that interfaces with
+lagann.
+*/
+package datatypes
