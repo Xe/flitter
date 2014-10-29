25ed03163c1764952d2a69db895ee284fdeccae8
Sam Dodrill <xena@yolo-swag.com>
2014-10-28 19:07:54 -0700

    cloudchaser: check permissions

---
+++ builder/cloudchaser/config.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"github.com/Xe/flitter/lib/etcdconfig"
+	"github.com/coreos/go-etcd/etcd"
+)
+
+// Struct Config holds all the configuration that builder will need in the form of
+// strings from etcd. This uses code in another part of the repository to marshall
+// and demarshall config in and out of etcd.
+type Config struct {
+	etcd *etcd.Client
+
+	RegistryHost string `etcd:"/flitter/registry/host"`
+	RegistryPort string `etcd:"/flitter/registry/port"`
+	LagannHost   string `etcd:"/flitter/lagann/host"`
+	LagannPort   string `etcd:"/flitter/lagann/port"`
+}
+
+// NewConfig allocates and retuens a config structure for builder. It also seeds
+// the values from etcd.
+func NewConfig(uplink string) (c *Config) {
+	c = &Config{
+		etcd: etcd.NewClient([]string{uplink}),
+	}
+	etcdconfig.Demarshal(c.etcd, c)
+	return
+}
--- builder/cloudchaser/main.go
+++ builder/cloudchaser/main.go
@@ -1,11 +1,20 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
+	"flag"
 	"fmt"
 	"log"
+	"net/http"
 	"os"
 
+	"github.com/Xe/flitter/lagann/datatypes"
 	"github.com/Xe/flitter/lib/output"
+)
+
+var (
+	etcdmachine = flag.String("-etcd", "http://172.17.42.1:4001", "etcd uplink")
 )
 
 func main() {
@@ -13,18 +22,59 @@ func main() {
 		log.Fatalln("Usage:\n  cloudchaser <revision> <sha>")
 	}
 
-	revision := os.Args[1]
-	sha := os.Args[2]
+	config := NewConfig(*etcdmachine)
 
-	output.WriteHeader("Receiver")
+	app := os.Getenv("REPO")
+	user := os.Getenv("USER")
 
-	output.WriteHeader("Environment:")
+	output.WriteHeader("Checking permission")
+	output.WriteData("user:   " + user)
+	output.WriteData("app:    " + app)
 
-	for _, val := range os.Environ() {
-		output.WriteData(val)
+	posturl := fmt.Sprintf("http://%s:%s/candeploy/%s", config.LagannHost,
+		config.LagannPort, app)
+
+	userob := &datatypes.User{
+		Name: user,
+		SSHKeys: []*datatypes.SSHKey{
+			{
+				Comment:     "cloudchaser!",
+				Key:         os.Getenv("KEY"),
+				Fingerprint: os.Getenv("FINGERPRINT"),
+			},
+		},
 	}
 
-	output.WriteHeader("Kicking off build")
-	output.WriteData(fmt.Sprintf("Revision: %s", revision))
-	output.WriteData(fmt.Sprintf("Sha: %s", sha))
+	jsondata, err := json.Marshal(userob)
+	if err != nil {
+		output.WriteError("Json encoding error")
+		output.WriteData("Please contact support")
+		os.Exit(1)
+	}
+
+	buf := bytes.NewBuffer(jsondata)
+
+	resp, err := http.Post(posturl, "application/json", buf)
+	if err != nil {
+		output.WriteError("Error in pemissions check: " + err.Error())
+		output.WriteData("Please make sure you have the requisite permissions to")
+		output.WriteData("deploy to this app.")
+		os.Exit(1)
+	}
+
+	if resp.StatusCode != 200 {
+		output.WriteError(fmt.Sprintf("Error: %d", resp.StatusCode))
+		output.WriteData(resp.Status)
+
+		if resp.StatusCode == 404 {
+			output.WriteData("Please recreate " + app + " and try again")
+		} else {
+			output.WriteData("Please contact support")
+		}
+
+		os.Exit(1)
+	}
+
+	os.Exit(0)
+
 }
