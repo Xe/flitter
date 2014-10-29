4146d1c3c78daeae95d77239602350927d0cfae7
Sam Dodrill <xena@yolo-swag.com>
2014-10-29 10:56:31 -0700

    lib/deployment: add documentation stub

---
+++ lib/deployment/doc.go
@@ -0,0 +1,7 @@
+/*
+Package deployment is a set of interfaces and wrappers around Docker, Fleet and
+other scheduling backends to allow for a generic method of submitting jobs.
+
+This package aims to not reinvent the wheel without good reason.
+*/
+package deployment
