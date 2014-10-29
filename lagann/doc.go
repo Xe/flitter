e5b8527afd5cf4c2c020023b67e670a0370a6758
Sam Dodrill <xena@yolo-swag.com>
2014-10-29 10:45:21 -0700

    lagann: add documentation

---
+++ lagann/doc.go
@@ -0,0 +1,7 @@
+/*
+Command lagann is Flitter's controller. It listens on port 3000 for HTTP traffic
+and all of its configuration is done via environment variables.
+
+HOST will set the host it attempts to connect to for etcd traffic.
+*/
+package main
