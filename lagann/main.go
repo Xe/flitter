59a589fd1c327668632802cf17258bd6c008f410
Sam Dodrill <xena@yolo-swag.com>
2014-10-29 22:32:31 -0700

    lagann: use auth, break into several servemuxes
    Giant commit for good reason
    
    All routed HandleFuncs are in a file based on if they are prelogin or
    postlogin.

--- lagann/main.go
+++ lagann/main.go
@@ -1,15 +1,10 @@
 package main
 
 import (
-	"encoding/json"
-	"io/ioutil"
 	"net/http"
 	"os"
-	"strings"
 
-	"code.google.com/p/go-uuid/uuid"
-	"github.com/Xe/dockerclient"
-	"github.com/Xe/flitter/lagann/datatypes"
+	"github.com/Xe/flitter/lib/middlewares/auth"
 	"github.com/codegangsta/negroni"
 	"github.com/coreos/go-etcd/etcd"
 	"github.com/coreos/go-systemd/unit"
@@ -17,192 +12,43 @@ import (
 	"gopkg.in/unrolled/render.v1"
 )
 
-// main is the entry point for lagann
-func main() {
-	r := render.New(render.Options{})
-	mux := routes.New()
+var r *render.Render
+var client *etcd.Client
 
-	client := etcd.NewClient([]string{"http://" + os.Getenv("HOST") + ":4001"})
-
-	mux.Get("/", func(w http.ResponseWriter, req *http.Request) {
-		reply(r, w, "No method", 500)
-	})
-
-	mux.Post("/register", func(w http.ResponseWriter, req *http.Request) {
-		user := &datatypes.User{}
-		body, err := ioutil.ReadAll(req.Body)
-		if err != nil {
-			reply(r, w, "Invalid request: "+err.Error(), 500)
-		}
-		err = json.Unmarshal(body, user)
-		if err != nil {
-			reply(r, w, "Invalid request: "+err.Error(), 500)
-		}
-
-		if _, err := client.Get("/flitter/builder/users/"+user.Name, false, false); err == nil {
-			reply(r, w, "User "+user.Name+" already exists", 409)
-		} else {
-			for _, key := range user.SSHKeys {
-				client.Set("/flitter/builder/users/"+user.Name+"/"+key.Fingerprint, key.Key, 0)
-			}
-
-			reply(r, w, "User "+user.Name+" created.", 200)
-		}
-	})
-
-	mux.Post("/create", func(w http.ResponseWriter, req *http.Request) {
-		app := &datatypes.App{}
-		body, err := ioutil.ReadAll(req.Body)
-		if err != nil {
-			reply(r, w, "Invalid request: "+err.Error(), 500)
-			return
-		}
-		err = json.Unmarshal(body, app)
-		if err != nil {
-			reply(r, w, "Invalid request: "+err.Error(), 500)
-			return
-		}
-
-		if _, err := client.Get("/flitter/apps/"+app.Name, false, false); err == nil {
-			reply(r, w, "App "+app.Name+" already exists", 409)
-			return
-		} else {
-			out, err := json.Marshal(app.Users)
-			if err != nil {
-				reply(r, w, "Invalid request: "+err.Error(), 500)
-				return
-			}
-			client.Set("/flitter/apps/"+app.Name+"/users", string(out), 0)
-			client.Set("/flitter/apps/"+app.Name+"/name", app.Name, 0)
-
-			reply(r, w, "App "+app.Name+" created", 200)
-		}
-	})
-
-	mux.Post("/candeploy/:app", func(w http.ResponseWriter, req *http.Request) {
-		params := req.URL.Query()
-		appname := params.Get(":app")
-
-		user := &datatypes.User{}
-		body, err := ioutil.ReadAll(req.Body)
-		if err != nil {
-			reply(r, w, "Invalid request: "+err.Error(), 500)
-			return
-		}
-		err = json.Unmarshal(body, user)
-		if err != nil {
-			reply(r, w, "Invalid request: "+err.Error(), 500)
-			return
-		}
-
-		var allowedusers []string
-
-		// Get app allowed users
-		res, err := client.Get("/flitter/apps/"+appname+"/users", false, false)
-		rawusers := res.Node.Value
-
-		err = json.Unmarshal([]byte(rawusers), &allowedusers)
-		if err != nil {
-			reply(r, w, "Internal json decoding reply in allowed app users parsing", 500)
-			return
-		}
-
-		for _, username := range allowedusers {
-			if strings.ToLower(username) == strings.ToLower(user.Name) {
-				reply(r, w, username+" is allowed", 200)
-				return
-			}
-		}
-
-		reply(r, w, "User is not authorized to make builds", 401)
-	})
-
-	mux.Post("/deploy/:app", func(w http.ResponseWriter, req *http.Request) {
-		params := req.URL.Query()
-		appname := params.Get(":app")
-
-		// Get build object
-		build := &datatypes.Build{}
-
-		body, err := ioutil.ReadAll(req.Body)
-		if err != nil {
-			reply(r, w, "Invalid request: "+err.Error(), 400)
-			return
-		}
-		err = json.Unmarshal(body, build)
-		if err != nil {
-			reply(r, w, "Invalid request: "+err.Error(), 400)
-			return
-		}
-
-		if _, err := client.Get("/flitter/apps/"+appname, false, false); err != nil {
-			reply(r, w, "No such app "+appname, 404)
-			return
-		} else {
-			// Do fleet deploy here
-
-			uid := uuid.New()[0:8]
-
-			unitSlice := []*unit.UnitOption{
-				{"Unit", "Description", "Flitter app " + appname + " deploy " + uid},
-				{"Service", "EnvironmentFile", "/etc/environment"},
-				{"Service", "ExecStartPre", "/usr/bin/docker pull " + build.Image},
-				{"Service", "ExecStartPre", "-/usr/bin/docker rm -f app-" + appname + "-" + build.ID + "-%i"},
-				{"Service", "ExecStart", "/bin/sh -c '/usr/bin/docker run -P --name app-" + appname + "-" + build.ID + "-%i --hostname " + appname + " -e HOST=$COREOS_PRIVATE_IPV4 " + build.Image + " '"},
-				{"Service", "ExecStop", "/usr/bin/docker rm -f app-" + appname + "-" + build.ID + "-%i"},
-			}
-
-			/*for startUnit("app-"+appname+"@"+uid, myunit) != nil {
-				log.Println("Trying to launch app-" + appname + "@" + uid + "...")
-			}*/
-
-			client, err := dockerclient.NewDockerClient("unix:///var/run/docker.sock")
-			if err != nil {
-				reply(r, w, "Can't make container", 500, err.Error())
-				return
-			}
-
-			hc := dockerclient.HostConfig{
-				PublishAllPorts: true,
-			}
-
-			splitimage := strings.Split(build.Image, "/")
-			tag := strings.Split(splitimage[len(splitimage)-1], ":")[1]
-			image := strings.Split(build.Image, ":"+tag)[0]
-
-			err = client.PullImage(image, tag)
-			if err != nil {
-				reply(r, w, "Can't pull image", 500, err.Error())
-			}
-
-			id, err := client.CreateContainer(&dockerclient.ContainerConfig{
-				Hostname:   appname,
-				Image:      build.Image,
-				HostConfig: hc,
-			}, "app-"+appname+"-"+build.ID+"-"+uid)
-			if err != nil {
-				reply(r, w, "Can't make container", 500, err.Error())
-				return
-			}
-			client.StartContainer(id, &hc)
-
-			reply(r, w, "App "+appname+" deployed", 200, unitSlice)
-		}
-	})
-
-	n := negroni.Classic()
-	n.UseHandler(mux)
-	n.Run(":3000")
+func init() {
+	r = render.New(render.Options{})
+	client = etcd.NewClient([]string{"http://" + os.Getenv("HOST") + ":4001"})
 }
 
-// reply takes in a render instance, an http responsewriter, a message string, http status code
-// and additional data to return to the user. This needs a "return" call after being run.
-func reply(r *render.Render, w http.ResponseWriter, message string, code int, data ...interface{}) {
-	r.JSON(w, code, map[string]interface{}{
-		"message": message,
-		"code":    code,
-		"data":    data,
-	})
+// main is the entry point for lagann
+func main() {
+	routing := http.NewServeMux()
+	usermux := routes.New()
+	appmux := routes.New()
+
+	n := negroni.Classic()
+	n.UseHandler(routing)
+
+	routing.HandleFunc("/", root)
+	routing.HandleFunc("/register", register)
+	routing.HandleFunc("/login", login)
+
+	usermux.Post("/user/create", createApp)
+	appmux.Post("/app/candeploy/:app", canDeployApp)
+	appmux.Post("/app/deploy/:app", deployApp)
+
+	auth, _ := auth.NewAuth("http://"+os.Getenv("HOST")+":4001", "/flitter/lagann/authkeys/")
+
+	routing.Handle("/user", negroni.New(
+		auth,
+		negroni.Wrap(usermux),
+	))
+
+	routing.Handle("/app", negroni.New(
+		negroni.Wrap(appmux),
+	))
+
+	n.Run(":3000")
 }
 
 func startUnit(name string, myunit []*unit.UnitOption) (err error) {
---
+++ lagann/preloginviews.go
@@ -0,0 +1,100 @@
+package main
+
+import (
+	"encoding/base64"
+	"net/http"
+
+	"code.google.com/p/go-uuid/uuid"
+
+	"github.com/Xe/flitter/lagann/datatypes"
+	"github.com/Xe/flitter/lib/utils"
+)
+
+// root is the handler for /. It is a simple 404 page.
+func root(w http.ResponseWriter, req *http.Request) {
+	reply(r, w, "No method", http.StatusNotFound)
+}
+
+func register(w http.ResponseWriter, req *http.Request) {
+	if req.Method != "POST" {
+		utils.Reply(r, w, "Invalid method", http.StatusMethodNotAllowed)
+		return
+	}
+
+	err := req.ParseForm()
+	if err != nil {
+		utils.Reply(r, w, "Could not parse form "+err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	password := utils.HashPassword([]byte(req.Form.Get("password")), []byte("lagann"))
+
+	user := &datatypes.User{
+		Name:     req.Form.Get("username"),
+		Password: string(password),
+		SSHKeys: []*datatypes.SSHKey{
+			&datatypes.SSHKey{
+				Key:         req.Form.Get("sshkey"),
+				Fingerprint: utils.GetFingerprint(req.Form.Get("sshkey")),
+			},
+		},
+	}
+
+	client.Set("/flitter/lagann/users/"+user.Name+"/password",
+		base64.StdEncoding.EncodeToString(password), 0)
+
+	if _, err := client.Get("/flitter/builder/users/"+user.Name, false, false); err == nil {
+		reply(r, w, "User "+user.Name+" already exists", 409)
+	} else {
+		for _, key := range user.SSHKeys {
+			client.Set("/flitter/builder/users/"+user.Name+"/"+key.Fingerprint, key.Key, 0)
+		}
+
+		authkey := uuid.New()
+
+		client.Set("/flitter/lagann/authkeys/"+authkey, user.Name, 0)
+
+		utils.Reply(r, w, "User "+user.Name+" created.", 200, map[string]interface{}{
+			"authkey": authkey,
+			"user":    user.Name,
+		})
+	}
+}
+
+func login(w http.ResponseWriter, req *http.Request) {
+	utils.Reply(r, w, "Not implemented", http.StatusNotImplemented)
+	return
+
+	if req.Method != "POST" {
+		utils.Reply(r, w, "Invalid method", http.StatusMethodNotAllowed)
+		return
+	}
+
+	err := req.ParseForm()
+	if err != nil {
+		utils.Reply(r, w, "Could not parse form "+err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	password := utils.HashPassword([]byte(req.Form.Get("password")), []byte("lagann"))
+	username := req.Form.Get("username")
+
+	comppw := base64.StdEncoding.EncodeToString(password)
+
+	storedpwnode, err := client.Get("/flitter/lagann/users/"+username+"/password", false, false)
+	if err != nil {
+		utils.Reply(r, w, "No such user "+username, http.StatusNotFound)
+	}
+
+	if storedpwnode.Node.Value == comppw {
+		authkey := uuid.New()
+
+		client.Set("/flitter/lagann/authkeys/"+authkey, username, 0)
+
+		utils.Reply(r, w, "Logged in as "+username+".", 200, map[string]interface{}{
+			"authkey": authkey,
+			"user":    username,
+		})
+
+	}
+}
