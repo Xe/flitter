3db05dd6aa883365d150e5f309db03e1dab99c99
Sam Dodrill <xena@yolo-swag.com>
2014-10-29 19:55:22 -0700

    middlewares/auth: document all functions better

--- lib/middlewares/auth/auth.go
+++ lib/middlewares/auth/auth.go
@@ -13,6 +13,10 @@ type Auth struct {
 	HeaderName string
 }
 
+// NewAuth returns a new Auth instance and an error or nil based on the machine and
+// the base path in etcd to look through. A sane default for Flitter is:
+//
+//    /flitter/lagann/apikeys/
 func NewAuth(machine, path string) (*Auth, error) {
 	a := &Auth{
 		e:          etcd.NewClient([]string{machine}),
@@ -23,6 +27,8 @@ func NewAuth(machine, path string) (*Auth, error) {
 	return a, nil
 }
 
+// This middleware will set the X-Lagann-User to the correct value out of etcd
+// based on the API key in X-Lagann-Auth.
 func (a *Auth) ServeHTTP(rw http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
 	authkey := r.Header.Get(a.HeaderName)
 	if authkey == "" {
@@ -30,7 +36,7 @@ func (a *Auth) ServeHTTP(rw http.ResponseWriter, r *http.Request, next http.Hand
 		return
 	}
 
-	node, err := a.e.Get("/flitter/lagann/authkeys/"+authkey, false, false)
+	node, err := a.e.Get(a.BasePath+authkey, false, false)
 	if err != nil {
 		http.Error(rw, `{"code": 401, "message": "Not authorized", "data": ["No user found for that API key."]}`, http.StatusUnauthorized)
 		return
