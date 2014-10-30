05d3d9160b45a0c39b113ae2eac8e4affcbd857a
Sam Dodrill <xena@yolo-swag.com>
2014-10-29 19:24:47 -0700

    lagann/middlewares: add etcd ssh key auth middleware

---
+++ lagann/middlewares/auth/auth.go
@@ -0,0 +1,39 @@
+package auth
+
+import (
+	"net/http"
+
+	"github.com/Xe/flitter/lib/utils"
+	"github.com/coreos/go-etcd/etcd"
+)
+
+// Struct Auth is a negroni middleware for checking X-Lagann-Auth for the correct SSH
+// key fingerprint.
+type Auth struct {
+	e          *etcd.Client
+	BasePath   string
+	HeaderName string
+}
+
+func NewAuth(machine, path string) (*Auth, error) {
+	a := &Auth{
+		e:          etcd.NewClient([]string{machine}),
+		BasePath:   path,
+		HeaderName: "X-Lagann-Auth",
+	}
+
+	return a, nil
+}
+
+func (a *Auth) ServeHTTP(rw http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
+	authkey := r.Header.Get(a.HeaderName)
+	if authkey == "" {
+		http.Error(rw, `{"code": 401, "message": "Not authorized", "data": ["Try setting `+a.HeaderName+`"]}`, http.StatusUnauthorized)
+	}
+
+	if user, allowed := utils.CanConnect(a.e, authkey); allowed {
+		r.Header.Set("X-Lagann-User", user)
+
+		next(rw, r)
+	}
+}
---
+++ lagann/middlewares/auth/doc.go
@@ -0,0 +1,5 @@
+/*
+Package auth is a middleware handler for Lagann to authenticate users
+over the SSH key they specified in the X-Lagann-Auth header.
+*/
+package auth
