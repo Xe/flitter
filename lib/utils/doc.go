872b8d27afa54552c543af2dc16c2359afff2226
Sam Dodrill <xena@yolo-swag.com>
2014-10-30 00:02:36 -0700

    lib/utils: add package documentation

---
+++ lib/utils/doc.go
@@ -0,0 +1,6 @@
+/*
+Package utils is for various "other" functions that things in Flitter use
+that may be useful in other places but are not unique or special enough to
+be in their own package.
+*/
+package utils
