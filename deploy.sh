ed6764f68a17891d294e19d8b60e2fa2651a12f2
Sam Dodrill <xena@yolo-swag.com>
2014-10-30 19:30:27 -0700

    systemd unit improvments
     - Use a generic announce@ service instead of service-specific ones
     - don't start flitter-proxy until flitter-builder is loaded
    
    Modify deployment shell script to reflect this

--- deploy.sh
+++ deploy.sh
@@ -4,15 +4,18 @@ set -ex
 
 # deploy flitter
 fleetctl start ./run/units/flitter-registry.service
-fleetctl start ./run/units/flitter-registry-announce.service
+fleetctl start ./run/units/flitter-announce@registry.service
 
 fleetctl start ./run/units/flitter-lagann.service
-fleetctl start ./run/units/flitter-lagann-announce.service
+fleetctl start ./run/units/flitter-announce@lagann.service
 
 fleetctl start ./run/units/flitter-builder.service
-fleetctl start ./run/units/flitter-builder-announce.service
+fleetctl start ./run/units/flitter-announce@builder.service
 
 fleetctl start ./run/units/flitter-havok.service
 
 # Routing
 fleetctl start ./run/units/flitter-router@1.service
+
+# Proxy
+fleetctl start ./run/units/flitter-proxy.service
