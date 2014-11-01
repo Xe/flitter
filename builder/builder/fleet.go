33d9999d0f7d8450ab57ad8704173cf0a70765c8
Sam Dodrill <xena@yolo-swag.com>
2014-10-31 19:14:50 -0700

    builder: use fleetctl

--- builder/builder/fleet.go
+++ builder/builder/fleet.go
@@ -1,32 +1,40 @@
 package main
 
 import (
-	"bytes"
-	"encoding/json"
-	"errors"
-	"net/http"
+	"io/ioutil"
+	"os/exec"
 
+	"github.com/Xe/flitter/lib/output"
 	"github.com/coreos/go-systemd/unit"
 )
 
 var unitTemplate = []*unit.UnitOption{}
 
 func startUnit(name string, myunit []*unit.UnitOption) (err error) {
-	outmap := map[string]interface{}{
-		"desiredState": "launched",
-		"options":      myunit,
+	dir, err := ioutil.TempDir("", "flitter-builder")
+	if err != nil {
+		return err
 	}
 
-	jsonstr, _ := json.Marshal(outmap)
-
-	client := &http.Client{}
-	req, err := http.NewRequest("PUT", "unix:///fleet.sock/v1-alpha/units/"+name+".service",
-		bytes.NewBuffer(jsonstr))
-
-	resp, err := client.Do(req)
-	if resp.StatusCode != 200 {
-		err = errors.New(resp.Status)
+	byteslicedunitreader := unit.Serialize(myunit)
+	byteslicedunit, err := ioutil.ReadAll(byteslicedunitreader)
+	if err != nil {
+		return err
 	}
 
-	return
+	err = ioutil.WriteFile(dir+"/"+name, byteslicedunit, 0666)
+	if err != nil {
+		return err
+	}
+
+	cmd := exec.Command("fleetctl", "--endpoint", *etcdhost, "start",
+		dir+"/"+name)
+
+	out, err := cmd.CombinedOutput()
+	if err != nil {
+		output.WriteData(string(out))
+		return err
+	}
+
+	return err
 }
