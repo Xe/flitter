54f89327e9477636a3006029b4a4c07c16d6e2f0
Sam Dodrill <xena@yolo-swag.com>
2014-11-14 09:54:39 -0800

    deploy.sh: make starting fleet units more robust

--- deploy.sh
+++ deploy.sh
@@ -1,35 +1,44 @@
-#!/bin/sh -e
-if ! etcdctl ${DEPLOY_PEERS/*/--peers $DEPLOY_PEERS} get /flitter/domain
+#!/bin/sh -ex
+
+if ! etcdctl get /flitter/domain
 then
 	echo "No domain set. Please set /flitter/domain."
 	exit 1
 fi
+
+function start-fleet-and-wait {
+  fleetctl load $*
+
+	for unit in $*
+	do
+		fleetctl ssh $unit sudo systemctl start $unit
+	done
+}
 
 # deploy flitter
 echo "Initiating the container announcer mesh"
 fleetctl start -block-attempts 30 ./run/units/flitter-havok.service
 
 echo "Deploying the builder"
-fleetctl start -block-attempts 30 ./run/units/flitter-builder.service
-fleetctl start -block-attempts 30 ./run/units/flitter-announce@builder.service
+start-fleet-and-wait ./run/units/flitter-builder.service
+start-fleet-and-wait ./run/units/flitter-announce@builder.service
 
 echo "Wiring up the builder proxy"
 fleetctl start -block-attempts 30 ./run/units/flitter-proxy.service
 
 echo "Spinning up Lagann"
-fleetctl start -block-attempts 30 ./run/units/flitter-lagann.service
-fleetctl start -block-attempts 30 ./run/units/flitter-announce@lagann.service
+start-fleet-and-wait ./run/units/flitter-lagann.service
+start-fleet-and-wait ./run/units/flitter-announce@lagann.service
 
 echo "Starting the docker registry"
-fleetctl start -block-attempts 30 ./run/units/flitter-registry.service
-fleetctl start -block-attempts 30 ./run/units/flitter-announce@registry.service
+start-fleet-and-wait ./run/units/flitter-registry.service
+start-fleet-and-wait ./run/units/flitter-announce@registry.service
 
 # Routing
 echo "Situating the routers"
-ROUTES=$(fleetctl list-machines -no-legend | awk -F'.' '{printf $1"\n"}')
-for n in $ROUTES
+for n in $(fleetctl list-machines -no-legend | awk -F'.' '{printf $1"\n"}')
 do
-	fleetctl start -block-attempts 30 ./run/units/flitter-router@"$n".service
+	start-fleet-and-wait ./run/units/flitter-router@"$n".service
 done
 
 echo "Flitter is deployed."
