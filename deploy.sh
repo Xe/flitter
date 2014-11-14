de8f3bb2095590c9831a1714f104268900e420e5
Sam Dodrill <xena@yolo-swag.com>
2014-11-14 10:19:14 -0800

    deploy.sh: make more robust

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,12 @@
 #!/bin/sh -ex
 
-if ! etcdctl get /flitter/domain
+if ! etcdctl --peers http://$FLEETCTL_TUNNEL:4001 get /flitter/domain
 then
 	echo "No domain set. Please set /flitter/domain."
 	exit 1
 fi
 
-function start-fleet-and-wait {
+function fleet_start_wait {
   fleetctl load $*
 
 	for unit in $*
@@ -15,30 +15,32 @@ function start-fleet-and-wait {
 	done
 }
 
+cd ./run/units/
+
 # deploy flitter
 echo "Initiating the container announcer mesh"
-fleetctl start -block-attempts 30 ./run/units/flitter-havok.service
+fleetctl start -block-attempts 30 flitter-havok.service
 
 echo "Deploying the builder"
-start-fleet-and-wait ./run/units/flitter-builder.service
-start-fleet-and-wait ./run/units/flitter-announce@builder.service
+fleet_start_wait flitter-builder.service
+fleet_start_wait flitter-announce@builder.service
 
 echo "Wiring up the builder proxy"
-fleetctl start -block-attempts 30 ./run/units/flitter-proxy.service
+fleetctl start -block-attempts 30 flitter-proxy.service
 
 echo "Spinning up Lagann"
-start-fleet-and-wait ./run/units/flitter-lagann.service
-start-fleet-and-wait ./run/units/flitter-announce@lagann.service
+fleet_start_wait flitter-lagann.service
+fleet_start_wait flitter-announce@lagann.service
 
 echo "Starting the docker registry"
-start-fleet-and-wait ./run/units/flitter-registry.service
-start-fleet-and-wait ./run/units/flitter-announce@registry.service
+fleet_start_wait flitter-registry.service
+fleet_start_wait flitter-announce@registry.service
 
 # Routing
 echo "Situating the routers"
 for n in $(fleetctl list-machines -no-legend | awk -F'.' '{printf $1"\n"}')
 do
-	start-fleet-and-wait ./run/units/flitter-router@"$n".service
+	fleet_start_wait flitter-router@"$n".service
 done
 
 echo "Flitter is deployed."
